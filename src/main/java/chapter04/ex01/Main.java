20524f74683d2f1a5f773c223fdafffc7aaa3ce4
wuhengflykite <705134043@qq.com>
2018-11-05 13:29:06 +0800

    Balking模式

---
+++ src/main/java/chapter04/ex01/Main.java
@@ -0,0 +1,9 @@
+package chapter04.ex01;
+
+public class Main {
+    public static void main(String[] args) {
+        Data data = new Data("data.txt", "(empty)");
+        new ChangerThread("ChangerThread", data).start();
+        new SaverThread("SaverThread", data).start();
+    }
+}
